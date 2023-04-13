0f64b25793ea24d2441aaf15dd13861b0ec1c3ea
tu6ge <tu6ge@qq.com>
2023-04-13 17:14:42 +0800

    feat(derive): remove #[derive(CustomItemError)]
    remove array2query

--- oss_derive/src/derive.rs
+++ oss_derive/src/derive.rs
@@ -1,14 +1,6 @@
 use proc_macro::TokenStream;
 use quote::quote;
 
-pub(crate) fn impl_custom_item_error(ast: &syn::DeriveInput) -> TokenStream {
-    let name = &ast.ident;
-    let gen = quote! {
-        impl aliyun_oss_client::decode::ItemError for #name {}
-    };
-    gen.into()
-}
-
 pub(crate) fn impl_custom_list_error(ast: &syn::DeriveInput) -> TokenStream {
     let name = &ast.ident;
     let gen = quote! {
