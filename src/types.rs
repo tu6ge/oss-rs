2ea507c7ca7ba57486e24ebb2217ac50d415139e
tu6ge <tu6ge@qq.com>
2023-07-10 09:29:48 +0800

    fix(types): changed BucketName default fn

--- src/types.rs
+++ src/types.rs
@@ -691,8 +691,8 @@ impl Display for BucketName {
 }
 
 impl Default for BucketName {
-    #![allow(clippy::unwrap_used)]
     fn default() -> BucketName {
+        #[allow(clippy::unwrap_used)]
         BucketName::new("a").unwrap()
     }
 }
