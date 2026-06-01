39524de45c0d264888b54974977e4c3dfc6e9752
tu6ge <tu6ge@qq.com>
2026-06-01 16:46:32 +0800

    feat(object_store): update BuiltinPutPayload to support streaming and add content_length method

--- crates/object_store/src/put_payload.rs
+++ crates/object_store/src/put_payload.rs
@@ -1,4 +1,5 @@
 use aliyun_oss_client::object::{BodyStream, IntoBody};
+use futures_util::stream;
 use object_store::PutPayload;
 
 pub(crate) struct BuiltinPutPayload(PutPayload);
@@ -11,6 +12,10 @@ impl BuiltinPutPayload {
 
 impl IntoBody for BuiltinPutPayload {
     fn into_body(self) -> BodyStream {
-        Box::pin(futures_util::stream::once(async move { Ok(self.0.into()) }))
+        Box::pin(stream::iter(self.0.into_iter().map(Ok)))
+    }
+
+    fn content_length(&self) -> Option<usize> {
+        Some(self.0.content_length())
     }
 }
