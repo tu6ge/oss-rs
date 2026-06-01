560cfbd868d3610a2115d3fced3ed7622da46cc5
tu6ge <tu6ge@qq.com>
2026-06-01 15:33:28 +0800

    feat(object_store): init ObjectStore implement

---
+++ crates/object_store/src/lib.rs
@@ -0,0 +1,68 @@
+use std::fmt::Display;
+
+use aliyun_oss_client::{Bucket, Client};
+use async_trait::async_trait;
+use futures_util::stream::BoxStream;
+use object_store::{
+    path::Path, CopyOptions, Error, GetOptions, GetResult, ListResult, MultipartUpload, ObjectMeta,
+    ObjectStore, PutMultipartOptions, PutOptions, PutPayload, PutResult, Result,
+};
+
+#[derive(Debug)]
+pub struct AliyunOssObjectStore {
+    client: Client,
+    bucket: Bucket,
+}
+
+impl Display for AliyunOssObjectStore {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "AliyunOssObjectStore")
+    }
+}
+
+impl AliyunOssObjectStore {
+    pub fn new(client: Client, bucket: Bucket) -> Self {
+        Self { client, bucket }
+    }
+}
+
+#[async_trait]
+impl ObjectStore for AliyunOssObjectStore {
+    async fn put_opts(
+        &self,
+        location: &Path,
+        payload: PutPayload,
+        opts: PutOptions,
+    ) -> Result<PutResult, Error> {
+        todo!()
+    }
+    async fn put_multipart_opts(
+        &self,
+        location: &Path,
+        opts: PutMultipartOptions,
+    ) -> Result<Box<dyn MultipartUpload>> {
+        todo!()
+    }
+    async fn get_opts(&self, location: &Path, opts: GetOptions) -> Result<GetResult, Error> {
+        todo!()
+    }
+
+    fn delete_stream(
+        &self,
+        locations: BoxStream<'static, Result<Path, Error>>,
+    ) -> BoxStream<'static, Result<Path, Error>> {
+        todo!()
+    }
+
+    fn list(&self, prefix: Option<&Path>) -> BoxStream<'static, Result<ObjectMeta>> {
+        todo!()
+    }
+
+    async fn list_with_delimiter(&self, prefix: Option<&Path>) -> Result<ListResult> {
+        todo!()
+    }
+
+    async fn copy_opts(&self, from: &Path, to: &Path, options: CopyOptions) -> Result<()> {
+        todo!()
+    }
+}
